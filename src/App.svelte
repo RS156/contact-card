712c38a00f6eb462be0172b21ac726f5406d4bad
RS156 <rishabh.sarkar0696@gmail.com>
2023-07-11 06:33:48 +0530

    added readme and configurable json file

--- src/App.svelte
+++ src/App.svelte
@@ -4,77 +4,16 @@
   import "@fortawesome/fontawesome-free/css/all.css";
   import "@fortawesome/fontawesome-free/js/all.js";
   import "./app.css";
+  import socialIconsInfo from "./assets/socialLinkIcons.json";
+  import contactLinksInfo from "./assets/contactCardInfo.json";
 
-  const socialIcons = [
-    {
-      bgColor: "bg-gray-800",
-      iconColor: "text-[#1773EA]",
-      iconClass: "fa-brands fa-facebook-f",
-      siteLink: "https://www.facebook.com/",
-    },
-    {
-      bgColor: "bg-gray-800",
-      iconColor: "text-[#1C9CEA]",
-      iconClass: "fa-brands fa-twitter",
-      siteLink: "https://www.twitter.com/",
-    },
-    {
-      bgColor: "bg-gray-800",
-      iconColor: "text-[#e14a84]",
-      iconClass: "fa-brands fa-dribbble",
-      siteLink: "https://dribbble.com/",
-    },
-    {
-      bgColor: "bg-gray-800",
-      iconColor: "text-[#0072b1]",
-      iconClass: "fa-brands fa-linkedin-in",
-      siteLink: "https://www.linkedin.com/",
-    },
-  ];
-
-  const contactLinks = [
-    {
-      label: "Phone",
-      info: "+123 456 789",
-      icon: {
-        bgColor: "bg-black",
-        iconColor: "text-[#E93B81]",
-        iconClass: "fa-solid fa-mobile-screen-button",
-      },
-    },
-    {
-      label: "Location",
-      info: "Hong kong china",
-      icon: {
-        bgColor: "bg-black",
-        iconColor: "text-[#6AB5B9]",
-        iconClass: "fa-solid fa-location-dot",
-      },
-    },
-    {
-      label: "Email",
-      info: "exampled@mail.com",
-      icon: {
-        bgColor: "bg-black",
-        iconColor: "text-[#FD7590]",
-        iconClass: "fa-solid fa-envelope",
-      },
-    },
-    {
-      label: "Birthday",
-      info: "May 27, 1990",
-      icon: {
-        bgColor: "bg-black",
-        iconColor: "text-[#C17CEB]",
-        iconClass: "fa-solid fa-calendar-days",
-      },
-    },
-  ];
+  const socialIcons = socialIconsInfo;
+  const contactLinks = contactLinksInfo;
 </script>
 
 <main>
   <div
-    class="px-6 rounded-[20px] mx-4 md:mx-auto max-w-md  mt-[180px] bg-gray-900 relative flex flex-col text-center mb-12 pb-4"
+    class="px-6 rounded-[20px] mx-4 md:mx-auto max-w-md mt-[180px] bg-gray-900 relative flex flex-col text-center mb-12 pb-4"
   >
     <img
       src={avatar}
@@ -104,8 +43,8 @@
                 ? ''
                 : 'border-gray-600 border-b'}"
             >
-            <div class=""><Icon {...contact.icon} /></div>
-              
+              <div class=""><Icon {...contact.icon} /></div>
+
               <div class="">
                 <p class="text-left text-sm text-gray-400">{contact.label}</p>
                 <p class="text-left text-base mt-1 break-all">{contact.info}</p>
@@ -114,7 +53,7 @@
           {/each}
         </div>
         <a
-          href='/contact.vcf'
+          href="/contact.vcf"
           download
           class="inline-flex items-center mx-auto bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-r hover:from-[#DD2476] hover:to-[#FA5252] px-8 py-3 text-xl text-white rounded-[35px] mt-8"
           >Download Contact Card</a
